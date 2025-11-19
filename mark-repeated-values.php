d50e8f7c7b1faa3d4a1d83a6bcd4c93d88a5a342
DungSaga <dungsaga@users.noreply.github.com>
2025-11-19 07:58:21 +0700

    only highlight values in the same column

--- mark-repeated-values.php
+++ mark-repeated-values.php
@@ -1,7 +1,7 @@
 <?php
 /**
-* Use alt+click to highlight the same values in other cells (up to 4 colors)
-* @link https://github.com/dungsaga/adminer-plugins/blob/main/mark-duplications.php
+* Use alt+click to highlight the same values in other cells of a column (up to 4 colors)
+* @link https://github.com/dungsaga/adminer-plugins/blob/main/mark-repeated-values.php
 * @author Dung.Saga, https://github.com/dungsaga
 * @license https://www.gnu.org/licenses/gpl-3.0.html GNU General Public License, version 3
 */
@@ -17,16 +17,16 @@ class AdminerMarkRepeatedValues extends Adminer\Plugin {
 	</style>
 	<script <?php echo Adminer\nonce(); ?>>
 	document.addEventListener('DOMContentLoaded', () => {
-		const classes = ['dups0', 'dups1', 'dups2', 'dups3',]
+		const classes = ['dups0', 'dups1', 'dups2', 'dups3',] // marker classes defined in CSS
 		let markerIdx = 0
 		qs('#table tbody').addEventListener('click', function(e) {
-			if (e.altKey && e.target.cellIndex) {
-				const markerClass = classes[markerIdx++ % classes.length]
-				qsa('td', this).forEach(td => {
-					td.classList.remove(markerClass)
+			if (e.altKey && e.target.cellIndex) { // when alt+click on a column
+				const col = e.target.cellIndex + 1
+				const markerClass = classes[markerIdx++ % classes.length] // select next marker class
+				qsa('td:nth-child('+col+')', this).forEach(td => { // check cells in a column
 					if (td.innerText === e.target.innerText) {
+						td.classList.remove(...classes)
 						td.classList.add(markerClass)
-						e.target.classList.add(markerClass)
 					}
 				})
 				e.stopPropagation()
